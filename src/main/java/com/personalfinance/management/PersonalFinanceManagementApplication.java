86177cb04dd3df4d2cd35b088fb9d8e67e7a74e1
mahfudnf <mahfudnf63@gmail.com>
2026-05-07 20:06:24 +0700

    api-personal-finance-management

--- src/main/java/com/personalfinance/management/PersonalFinanceManagementApplication.java
+++ src/main/java/com/personalfinance/management/PersonalFinanceManagementApplication.java
@@ -22,6 +22,7 @@ public class PersonalFinanceManagementApplication {
 			System.setProperty("DB_URL", dotenv.get("DB_URL"));
 			System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+			System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
 
 			SpringApplication.run(PersonalFinanceManagementApplication.class, args);
 		} catch (Exception e) {
